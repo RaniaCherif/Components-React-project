3b4b0502d77fc57ea3386861157f1f4dbcd1b5d3
rania <contactraniacherif@gmail.com>
2023-02-04 11:47:04 +0100

    add component profile

--- src/Profile.js
+++ src/Profile.js
@@ -14,7 +14,27 @@ import {
 const Profile = () => {
   return (
     <div className="vh-100" style={{ backgroundColor: "#9de2ff" }}>
-      
+      <MDBContainer>
+        <MDBRow className="justify-content-center">
+          <MDBCol md="9" lg="7" xl="5" className="mt-5">
+            <MDBCard style={{ borderRadius: "15px" }}>
+              <MDBCardBody className="p-4">
+                <div className="d-flex text-black">
+                  <div className="flex-shrink-0">
+                    <MDBCardImage
+                      style={{ width: "180px", borderRadius: "10px" }}
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                      alt="Generic placeholder image"
+                      fluid
+                    />
+                  </div>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
   );
 };
 
